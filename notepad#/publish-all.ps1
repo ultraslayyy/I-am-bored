d257ed02d496c177abe311aea6caa9f35e93c9e5
ultraslayyy <ultra@ultraslayyy.xyz>
2025-10-06 17:44:57 +1100

    notepad#: Fix publish-all.ps1

--- notepad#/publish-all.ps1
+++ notepad#/publish-all.ps1
@@ -1,8 +1,7 @@
 param(
     [switch]$SingleFile,
     [switch]$FrameworkDependent,
-    [switch]$MakeInstaller,
-    [switch]$FINAL
+    [switch]$MakeInstaller
 )
 
 Set-Location src
@@ -16,32 +15,31 @@ foreach ($pprofile in $profiles) {
         "WinArm64" { $platformFolder = "win-arm64" }
         default    { $platformFolder = $pprofile }
     }
-    
-    $publicArgs = @("/p:PublishProfile=$pprofile")
+
+    $publishArgs = @("/p:PublishProfile=$pprofile")
+
     if ($FrameworkDependent) {
        $publishArgs += "/p:SelfContained=false"
     } else {
         $publishArgs += "/p:SelfContained=true"
     }
+
     if ($SingleFile) {
         $publishArgs += "/p:PublishSingleFile=true"
         if (-not $FrameworkDependent) { $publishArgs += "/p:PublishTrimmed=true" }
     }
+
     $publishArgs += "/p:PublishReadyToRun=true"
-    Write-Host "Publishing $pprofile ($($publishArgs -join ', ')) into publish\$platformFolder..."
-    dotnet publish $publishArgs
+
+    Write-Host "Publishing $pprofile ($($publishArgs -join ' ')) into publish\$platformFolder..."
+    dotnet publish @publishArgs
 
     if ($MakeInstaller) {
         $installerPath = "..\installer"
-        if (-not (Test-Path "$installerPath/build")) { New-Item -ItemType Directory "$installerPath/build" }
+        if (-not (Test-Path "$installerPath/build")) { New-Item -ItemType Directory "$installerPath/build" | Out-Null }
         Push-Location ..\installer
-        if ($FINAL) {
-            Write-Host "Building installer for $pprofile into installer/Output (FINAL)..."
-            makensis /DPlatform=$platformFolder /DFINAL Installer.nsi
-        } else {
-            Write-Host "Building installer for $pprofile into installer/Output..."
-            makensis /DPlatform=$platformFolder Installer.nsi
-        }
+        Write-Host "Building installer for $pprofile into installer/Output..."
+        makensis /DPlatform=$platformFolder Installer.nsi
         Pop-Location
     }
 }
