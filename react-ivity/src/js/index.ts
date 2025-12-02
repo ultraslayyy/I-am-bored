b8fa70ef11f3abfccaf257d320f992834df68270
ultraslayyy <ultra@ultraslayyy.xyz>
2025-12-02 18:39:45 +1100

    react-ivity: Did smthn ig?

--- react-ivity/src/js/index.ts
+++ react-ivity/src/js/index.ts
@@ -1,12 +1,22 @@
 let activeEffect: (() => void) | null = null;
 const dependencyMap = new WeakMap();
 
+const globalHookState: any[] = [];
+let hookIndex = 0;
+
 type StateObject<T> = {
     value: T;
 }
 
 export function useState<T>(initialValue: T): StateObject<T> {
-    let value = initialValue;
+    const currentHookIndex = hookIndex;
+    hookIndex++;
+
+    if (globalHookState[currentHookIndex] !== undefined) {
+        return globalHookState[currentHookIndex] as StateObject<T>;
+    }
+
+    const stateContainer = { value: initialValue }
     
     const handler = {
         get(target: any, key: string | symbol) {
@@ -38,14 +48,53 @@ export function useState<T>(initialValue: T): StateObject<T> {
         }
     }
 
-    return new Proxy({ value }, handler);
+    const proxiedState = new Proxy(stateContainer, handler) as StateObject<T>;
+    globalHookState[currentHookIndex] = proxiedState;
+
+    return proxiedState;
 }
 
-export function useEffect(fn: () => void) {
+export function useEffect(fn: () => void, dependencyArray?: any[]) {
     const effect = () => {
+        hookIndex = 0;
+
         activeEffect = effect;
         fn();
         activeEffect = null;
     }
     effect();
+}
+
+export function useCallback<T extends Function>(callback: T, dependencies: any[]): T {
+    const currentHookIndex = hookIndex;
+    hookIndex++;
+
+    if (globalHookState[currentHookIndex] === undefined) {
+        globalHookState[currentHookIndex]= {
+            fn: callback,
+            deps: dependencies
+        }
+        return callback;
+    }
+
+    const cachedHook = globalHookState[currentHookIndex];
+    const previesDeps = cachedHook.deps;
+    let dependenciesChanged = previesDeps.length !== dependencies.length;
+
+    if (!dependenciesChanged) {
+        for (let i = 0; i < dependencies.length; i++) {
+            if (dependencies[i] !== previesDeps[i]) {
+                dependenciesChanged = true;
+                break;
+            }
+        }
+    }
+
+    if (dependenciesChanged) {
+        cachedHook.fn = callback;
+        cachedHook.deps = dependencies;
+        return callback;
+    } else {
+        return cachedHook.fn as T;
+    }
 }
